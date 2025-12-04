9bf3970336b0fae8d2ec08ff63161fec5db037e7
Stonedbybread <160148736+Stonedbybread@users.noreply.github.com>
2025-12-03 23:46:23 -0800

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = "yellow-cache-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./512.png",
+  "./192.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
